ce758a37fe6645edac9bfd3fedd134bad3cd4bb3
ekrem <ekremufacik07@gmail.com>
2024-03-27 20:53:25 +0300

    assets path edited

--- src/utils/weatherImages.ts
+++ src/utils/weatherImages.ts
@@ -1,33 +1,58 @@
+import bgCloudsDay from "@/assets/cloudy-day.png";
+import bgCloudsNight from "@/assets/cloudy-night.png";
+import iconCloudsDay from "@/assets/icons/cloudy-day.svg";
+import iconCloudsNight from "@/assets/icons/cloudy-night.svg";
+
+import bgRainDay from "@/assets/rain-day.png";
+import bgRainNight from "@/assets/rain-night.png";
+import iconRainDay from "@/assets/icons/rain-day.svg";
+import iconRainNight from "@/assets/icons/rain-night.svg";
+
+import bgClearDay from "@/assets/clear-day.png";
+import bgClearNight from "@/assets/clear-night.png";
+import iconClearDay from "@/assets/icons/clear-day.svg";
+import iconClearNight from "@/assets/icons/clear-night.svg";
+
+import bgSnowDay from "@/assets/snow-day.png";
+import bgSnowNight from "@/assets/snow-night.png";
+import iconSnowDay from "@/assets/icons/snow-day.svg";
+import iconSnowNight from "@/assets/icons/snow-night.svg";
+
+import bgStormDay from "@/assets/storm-day.png";
+import bgStormNight from "@/assets/storm-night.png";
+import iconStormDay from "@/assets/icons/storm-day.svg";
+import iconStormNight from "@/assets/icons/storm-night.svg";
+
 export const weatherImages = {
   Clouds: {
-    bg_day: "/src/assets/cloudy-day.png",
-    bg_night: "/src/assets/cloudy-night.png",
-    icon_day: "/src/assets/icons/cloudy-day.svg",
-    icon_night: "/src/assets/icons/cloudy-night.svg",
+    bg_day: bgCloudsDay,
+    bg_night: bgCloudsNight,
+    icon_day: iconCloudsDay,
+    icon_night: iconCloudsNight,
   },
   Rain: {
-    bg_day: "/src/assets/rain-day.png",
-    bg_night: "/src/assets/rain-night.png",
-    icon_day: "/src/assets/icons/rain-day.svg",
-    icon_night: "/src/assets/icons/rain-night.svg",
+    bg_day: bgRainDay,
+    bg_night: bgRainNight,
+    icon_day: iconRainDay,
+    icon_night: iconRainNight,
   },
   Clear: {
-    bg_day: "/src/assets/clear-day.png",
-    bg_night: "/src/assets/clear-night.png",
-    icon_day: "/src/assets/icons/clear-day.svg",
-    icon_night: "/src/assets/icons/clear-night.svg",
+    bg_day: bgClearDay,
+    bg_night: bgClearNight,
+    icon_day: iconClearDay,
+    icon_night: iconClearNight,
   },
   Snow: {
-    bg_day: "/src/assets/snow-day.png",
-    bg_night: "/src/assets/snow-night.png",
-    icon_day: "/src/assets/icons/snow-day.svg",
-    icon_night: "/src/assets/icons/snow-night.svg",
+    bg_day: bgSnowDay,
+    bg_night: bgSnowNight,
+    icon_day: iconSnowDay,
+    icon_night: iconSnowNight,
   },
   Storm: {
-    bg_day: "/src/assets/storm-day.png",
-    bg_night: "/src/assets/storm-night.png",
-    icon_day: "/src/assets/icons/storm-day.svg",
-    icon_night: "/src/assets/icons/storm-night.svg",
+    bg_day: bgStormDay,
+    bg_night: bgStormNight,
+    icon_day: iconStormDay,
+    icon_night: iconStormNight,
   },
 };
 
